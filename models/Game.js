02466727a68de0a44d3311a153d132259b341598
sahilthakur7 <sahilrd07@gmail.com>
2019-08-07 23:25:01 +0530

    started with the game model

---
+++ models/Game.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const gameSchema = new mongoose.Schema({
+    title: String,
+    rating: Number,
+    platforms: [String],
+    genre: String
+});
+
+mongoose.model('Game',gameSchema);
