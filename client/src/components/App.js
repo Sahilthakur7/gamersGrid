39e3164eecf0bc7fa5a50af55e7f5cbfef3cc791
sahilthakur7 <sahilrd07@gmail.com>
2019-08-08 22:45:40 +0530

    added navbar

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter , Link , Route } from 'react-router-dom';
 import Home from './Home';
+import Header from './Header';
 
 class App extends React.Component {
     render(){
         return(
             <div className="container-fluid">
+                <Header/>
                 <BrowserRouter>
                     <Route exact path="/" component={Home} />
                 </BrowserRouter>
