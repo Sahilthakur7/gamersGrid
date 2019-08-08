ff6b01a335a3214f26cccf6bee74f695b30bf166
sahilthakur7 <sahilrd07@gmail.com>
2019-08-08 23:16:00 +0530

    started with game form

---
+++ client/src/components/NewGameForm.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class NewGameForm extends React.Component {
+    state = {
+        title: '',
+        platforms: '',
+        genre: ''
+    };
+
+    render(){
+        return(
+            <div>
+            </div>
+        )
+    }
+}
