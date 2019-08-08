1c89fcfe5855b295b94136ce30a2fbc1e2b77389
sahilthakur7 <sahilrd07@gmail.com>
2019-08-08 23:00:37 +0530

    added google fonts

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -5,7 +5,7 @@ class Header extends React.Component {
     render(){
         return(
             <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                <Navbar.Brand href="#home" className="logo-text">GamersGrid</Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="mr-auto">
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import './assets/styles/main.css';
 import * as serviceWorker from './serviceWorker';
 
 
