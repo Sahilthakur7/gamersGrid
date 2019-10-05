00e91cd7ac5dc65b3ae93acfd9ed85602e0931ac
sahilthakur7 <sahilrd07@gmail.com>
2019-10-05 16:40:00 +0530

    loggin in the frontend

--- client/src/actions/usersActions.js
+++ client/src/actions/usersActions.js
@@ -3,11 +3,21 @@ import {
 } from '../services/userService';
 
 import {
-    AUTHENTICATED_USER
+    AUTHENTICATED_USER,
+    AUTHENTICATED_USER_FAILED
 } from '../reducers/usersReducer';
 
 export const authenticate = (user) => (dispatch) => {
     return authenticateUser(user).then(res => {
-        console.log(res);
+        dispatch({
+            type: AUTHENTICATED_USER,
+            payload: res
+        });
+
+    }).catch((err) => {
+        dispatch({
+            type: AUTHENTICATED_USER_FAILED,
+            payload: err
+        })
     })
 }
--- client/src/reducers/usersReducer.js
+++ client/src/reducers/usersReducer.js
@@ -1,12 +1,24 @@
 export const AUTHENTICATED_USER = 'AUTHENTICATED_USER';
+export const AUTHENTICATED_USER_FAILED = 'AUTHENTICATED_USER_FAILED';
 
 const INITIAL_STATE = {
     user: {
 
     },
+    message:'',
     error: ''
 };
 
 export default function(state = INITIAL_STATE,action){
     const {payload} = action;
+    let stateUpdatedObj;
+    switch(action.type){
+        case AUTHENTICATED_USER:
+            stateUpdatedObj = {
+                user: {
+                    ...payload.data
+                },
+                message: 'You have logged In'
+            }
+    };
 }
